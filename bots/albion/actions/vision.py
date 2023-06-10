7201d93d8e1169ea0fbe0baf77c5aa9508fc1288
Dmitry G <sdprecovery@gmail.com>
2023-06-10 07:34:51 +0300

    Update albion actions

---
+++ bots/albion/actions/vision.py
@@ -0,0 +1,24 @@
+from infra.common.entities import Coord, Img, Rect
+from infra.vision.opencv import OpenCV
+from infra.vision.utils import load_img
+
+CROPS = {
+    "SKILL_PANEL": Rect(Coord(480, 975), Coord(1475, 1065)),
+}
+
+
+def find_mount(search_img: Img):
+    ref_img = load_img("albion/ui/mount_hp_1.png")
+    result = OpenCV.find(ref_img, search_img, confidence=0.85)
+    return result
+
+
+def find_skill(search_img: Img, ref_name: str):
+    skills = {
+        "town_teleport": "albion/ui/skill_teleport.png",
+    }
+    ref_img = load_img(skills[ref_name])
+    result = OpenCV.find(
+        ref_img, search_img, confidence=0.85, crop=CROPS["SKILL_PANEL"]
+    )
+    return result
